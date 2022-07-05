79fa4f5d70e170e10fd559bfcd86e93887df4044
Zachary Soliman <zachsoliman1@gmail.com>
2022-07-05 10:13:07 -0400

    commit tree 348fdad743e36639252248ca150b14dca5811508

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+// alert('test')
+
+let hq = 42
+const distanceFromHqInBlocks = (location) => {
+    if (location > 42) {
+        return (location - hq)
+    } else {
+        return (hq - location)
+    }
+}
+
+const distanceFromHqInFeet = (location) => {
+    if (location > 42) {
+        return ((location - hq) * 264)
+    } else {
+        return ((hq - location) * 264)
+    }
+}
+
+const distanceTravelledInFeet = (start, destination) => {
+    return Math.abs((destination - start) * 264)
+}
+
+const calculatesFarePrice = (start, destination) => {
+    let distance = distanceTravelledInFeet(start, destination)
+    if (distance <= 400) {
+        return 0
+    } else if (distance <= 2000) {
+        return ((distance - 400) * .02)
+    } else if (distance < 2500) {
+        return 25
+    } else {
+        return 'cannot travel that far'
+    }
+}
